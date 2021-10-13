03e5ebdd50dd4af726c85ed6526af192f71fb85e
jessepierce451 <43769051+jessepierce451@users.noreply.github.com>
2021-10-13 11:28:40 -0500

    forgot to call providers

--- src/BonsaiBalance.Web/ClientApp/src/main.ts
+++ src/BonsaiBalance.Web/ClientApp/src/main.ts
@@ -16,5 +16,5 @@ if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
+platformBrowserDynamic(providers).bootstrapModule(AppModule)
   .catch(err => console.error(err));
