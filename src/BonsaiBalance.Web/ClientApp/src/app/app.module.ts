f0634a3bcd696542fdec492b49325e2b6276dbc9
Jesse Pierce <43769051+jessepierce451@users.noreply.github.com>
2021-10-27 22:20:46 -0500

    Appsettings Service (#9)
    * added appsettings service and updated app module
    
    * provided BASE_URL
    
    * forgot to call providers
    
    * fixed not a function bug, follows provider order
    
    * woops

--- src/BonsaiBalance.Web/ClientApp/src/app/app.module.ts
+++ src/BonsaiBalance.Web/ClientApp/src/app/app.module.ts
@@ -1,17 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PlaidLinkComponent } from './components/plaid-link/plaid-link.component';
 import { NgxPlaidLinkModule } from 'ngx-plaid-link';
-import { HttpClientModule } from '@angular/common/http';
 import { SideBarComponent } from './components/side-bar/side-bar.component';
+import { AppSettingsProviders } from './app.settings';
 
 @NgModule({
     declarations: [AppComponent, PlaidLinkComponent, SideBarComponent],
     imports: [BrowserModule, HttpClientModule, AppRoutingModule, NgxPlaidLinkModule],
-    providers: [],
+    providers: [AppSettingsProviders],
     bootstrap: [AppComponent]
 })
 export class AppModule {}
