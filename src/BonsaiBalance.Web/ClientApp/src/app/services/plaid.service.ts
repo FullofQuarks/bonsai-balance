534ffd025ffa046bed1e5662c5cee8cc65246bf7
Bryan Pham <bryan_pham123@yahoo.com>
2021-11-11 15:53:17 -0600

    Upgrade .net core 6

--- src/BonsaiBalance.Web/ClientApp/src/app/services/plaid.service.ts
+++ src/BonsaiBalance.Web/ClientApp/src/app/services/plaid.service.ts
@@ -2,19 +2,20 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { PlaidLinkTokenResponse } from '../common/models/plaid/plaid-link-token-response';
+import { AppSettingsService } from '../app.settings';
 
 @Injectable({
     providedIn: 'root'
 })
 export class PlaidService {
-    constructor(private httpService: HttpClient) {}
+    constructor(private httpService: HttpClient, private appSettings: AppSettingsService) {}
 
     public createLinkToken(): Observable<PlaidLinkTokenResponse> {
-        return this.httpService.get<PlaidLinkTokenResponse>('https://localhost:5020/plaid/token');
+        return this.httpService.get<PlaidLinkTokenResponse>(this.appSettings.settings.bonsaiBalanceApi + '/plaid/token');
     }
 
     public exchangeLinkToken(publicToken: string) {
-        return this.httpService.post<any>(`https://localhost:5020/plaid/token`, {
+        return this.httpService.post<any>(this.appSettings.settings.bonsaiBalanceApi + '/plaid/token', {
             publicToken: publicToken
         });
     }
