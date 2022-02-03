3bcf8c6c943baf7a75cefaa990818142f7924331
Nicholas Smith <nsmith@ethosgroup.com>
2022-02-03 17:41:27 -0600

    Init commit

---
+++ src/BonsaiBalance.Blazor/Models/BonsaiBalanceDatabaseSettings.cs
@@ -0,0 +1,8 @@
+namespace BonsaiBalance.Blazor.Models;
+
+public class BonsaiBalanceDatabaseSettings
+{
+    public string ConnectionString { get; set; } = null!;
+    public string DatabaseName { get; set; } = null!;
+    public string CollectionName { get; set; } = null!;
+}
