08bd0a948cf4c6194269fc39da014ab1711eaea7
Nicholas Smith <nsmith@ethosgroup.com>
2022-03-01 10:54:56 -0600

    Removed Angular web project

--- src/BonsaiBalance.Web/ClientApp/src/app/common/constants/side-bar-items.ts
+++
@@ -1,8 +0,0 @@
-export enum SideBarItems {
-    DASHBOARD = 'Dashboard',
-    TRANSACTIONS = 'Transactions',
-    ITEM3 = 'Item 3',
-    ITEM4 = 'Item 4',
-    ITEM5 = 'Item 5',
-    ITEM6 = 'Item 6'
-}
