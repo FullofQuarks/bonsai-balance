6b7b9005218e184b8f53e3b9be0243c8b4b2743d
jessepierce451 <43769051+jessepierce451@users.noreply.github.com>
2022-03-05 10:56:59 -0600

    more removal

--- BonsaiBalance.Web/ClientApp/src/App.js
+++ BonsaiBalance.Web/ClientApp/src/App.js
@@ -1,18 +1,15 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Route } from 'react-router';
 import Layout from './components/Layout';
 import Home from './components/Home';
 
 import './custom.css'
 
-export default class App extends Component {
-  static displayName = App.name;
-
-  render () {
+ const App = () => {
     return (
       <Layout>
         <Route exact path='/' component={Home} />
       </Layout>
-    );
-  }
+    )
 }
+export default App
