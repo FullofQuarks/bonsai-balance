d3f9b570834e47b18b208079ac46e5858f8541ec
jessepierce451 <43769051+jessepierce451@users.noreply.github.com>
2022-03-05 10:44:27 -0600

    changing components to functions

--- BonsaiBalance.Web/ClientApp/src/components/Layout.js
+++ BonsaiBalance.Web/ClientApp/src/components/Layout.js
@@ -1,18 +1,16 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Container } from 'reactstrap';
 import { NavMenu } from './NavMenu';
 
-export class Layout extends Component {
-  static displayName = Layout.name;
-
-  render () {
+const Layout = ({ children }) => {
     return (
       <div>
         <NavMenu />
         <Container>
-          {this.props.children}
+          {children}
         </Container>
       </div>
-    );
-  }
+    )
 }
+
+export default Layout
